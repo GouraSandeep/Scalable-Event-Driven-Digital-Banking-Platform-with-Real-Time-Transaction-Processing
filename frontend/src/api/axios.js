232e1584d3fa5d2614cbba4e9e1c264dad5cf791
GouraSandeep <gourasandeep10@gmail.com>
2026-03-17 21:44:24 +0530

    connect frontend to production backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://scalable-event-driven-digital-banking.onrender.com",
   withCredentials: true,
 });
 
