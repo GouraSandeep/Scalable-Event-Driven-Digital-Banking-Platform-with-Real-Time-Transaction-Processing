cf3cca05b165791d8733ef23c4212311571f3cfe
GouraSandeep <gourasandeep10@gmail.com>
2026-03-17 20:42:03 +0530

    move redis url to env for deployment

--- backend/queue/redisConnection.js
+++ backend/queue/redisConnection.js
@@ -1,11 +1,8 @@
 import IORedis from "ioredis";
 
-const connection = new IORedis(
-  "redis://default:s6xUHBOOcTe6IvfCT1crOzaT8b4v1DY8@redis-19744.crce281.ap-south-1-3.ec2.cloud.redislabs.com:19744",
-  {
-    maxRetriesPerRequest: null,
-  },
-);
+const connection = new IORedis(process.env.REDIS_URL, {
+  maxRetriesPerRequest: null,
+});
 
 connection.on("connect", () => {
   console.log(" Redis Connected");
