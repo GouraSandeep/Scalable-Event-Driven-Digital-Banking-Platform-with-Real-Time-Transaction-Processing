7bdf7c8271b941f2a36d4ddda5e6c830b8ee3735
GouraSandeep <gourasandeep10@gmail.com>
2026-03-17 22:17:00 +0530

    Fixing URI

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://scalable-event-driven-digital-banking.onrender.com",
+  baseURL: "https://scalable-event-driven-digital-banking.onrender.com/api",
   withCredentials: true,
 });
 
