e1d577e597356d1a4f3dfd864a9a22b7a17ee550
GouraSandeep <gourasandeep10@gmail.com>
2026-03-18 15:06:58 +0530

    fixing cookies issue

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -62,8 +62,9 @@ router.post("/login", async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     res.status(200).json({ message: "login success", user });
--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
+app.set("trust proxy", 1);
 
 app.use(
   cors({
